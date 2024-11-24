ba2e7792bbc4acb37e38f919df1274d8c09c3d40
binhdz1102 <husemail00@gmail.com>
2024-11-24 19:09:58 +0700

    test su dung codevista trong project android jetpack compose

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+rootProject.name = "AndroidCodeVista"
+
+include(":app")
+include(":app:testunittest")
